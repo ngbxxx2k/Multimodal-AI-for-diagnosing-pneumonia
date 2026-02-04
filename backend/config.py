544af75ed2a707aa2dd24062f552f5776ead3241
ngbxxx2k <baodt4549@gmail.com>
2026-02-04 19:25:07 +0700

    update model densenet

--- backend/config.py
+++ backend/config.py
@@ -12,7 +12,7 @@ PORT = int(os.getenv("PORT", 8000))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 UNET_PATH = os.getenv("UNET_PATH", os.path.join("model_ai", "best_lung_unet.pth"))
-DENSENET_PATH = os.getenv("DENSENET_PATH", os.path.join("model_ai", "best_softmax_lung_crop_stage3_final.keras"))
+DENSENET_PATH = os.getenv("DENSENET_PATH", os.path.join("model_ai", "best_binary_xray_recall98.keras"))
 YOLO_PATH = os.getenv("YOLO_PATH", os.path.join("model_ai", "best_RSNA_HIGH_LR.pt"))
 
 PNEUMONIA_THRES_HIGH = 80.0
